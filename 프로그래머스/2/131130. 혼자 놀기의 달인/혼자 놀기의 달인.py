20df65354eab8d4ec60dbabd0b52dea5bc6aa3e7
Seo-Kyeong Hwang <purewater71@naver.com>
2024-03-02 18:45:33 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.31 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
@@ -0,0 +1,41 @@
+import random
+
+def solution(cards):
+    answer = 0
+    isOpened = [False] * len(cards)
+    rest = []
+    score = []
+    
+    def openBox(box, boxNum):
+        cnt = 0
+        while not isOpened[box-1]:
+            isOpened[box-1] = True
+            cnt += 1
+            box = boxNum
+            boxNum = cards[box-1]
+        return cnt
+    
+    while False in isOpened:
+        if True in isOpened:
+            for i in range(len(isOpened)):
+                if not isOpened[i]:
+                    rest.append(i+1)
+            box = random.choice(rest)
+            boxNum = cards[box-1]
+            score.append(openBox(box, boxNum))
+        else:
+            box = random.randint(1, len(cards))
+            # 열어야 하는 상자가 이미 열려있을 때까지 반복
+            boxNum = cards[box-1]
+            score.append(openBox(box, boxNum))
+         
+    if len(score) == 1:
+        return 0
+    
+    score.sort()
+    answer = score[-1] * score[-2]
+    return answer
+
+    
+        
+    
