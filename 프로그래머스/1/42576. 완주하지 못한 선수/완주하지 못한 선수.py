c63f1ecc3dced6bb7340e35b5a5383fb9bee9a24
Seo-Kyeong Hwang <purewater71@naver.com>
2024-03-04 20:39:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 59.44 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,12 @@
+def solution(participant, completion):
+    dic1 = {i:0 for i in participant}
+    dic2 = {i:0 for i in participant}
+
+    for i in participant:
+        dic1[i] += 1
+    for i in completion:
+        dic2[i] += 1
+
+    for i in dic1:
+        if dic1[i] - dic2[i] == 1:
+            return i 
