975dd0cbffe224f8a46339366d2189b194c1d23d
Seo-Kyeong Hwang <purewater71@naver.com>
2024-03-06 16:29:23 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 628 ms, Memory: 71180 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10 ** 8)
+
+input = sys.stdin.readline
+
+n, m, s = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+cnt = 1
+
+def dfs(s, graph, visited):
+    global cnt
+    visited[s] = cnt
+
+    for i in graph[s]:
+        if not visited[i]:
+            cnt += 1
+            dfs(i, graph, visited)
+
+for i in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(len(graph)):
+    graph[i].sort(reverse=True)
+
+dfs(s, graph, visited)
+
+for i in visited[1:]:
+    print(i)
